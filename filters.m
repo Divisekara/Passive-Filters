396763b7781963506aa02637f2e91c63a849ed0c
Divisekara <9asitha7@gmail.com>
2020-05-31 12:55:22 +0530

    2

--- filters.m
+++ filters.m
@@ -1,12 +1,15 @@
-Clc;
+clc;clear;
 close all;
 s=tf('s');
+
 %let
 R=2;
 C=2e-6;
 L=1e-3;
+
 %required transfer function
 G=(s*L+(1/(s*C)))/(R+s*L+(1/(s*C)));
+w = 10 * 10^3;
 %given requirements
 %initialize the frequency range
 bode(G,w);
